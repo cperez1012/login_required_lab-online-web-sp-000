348bf27c25527250ce225ba673e67744bb30d46b
Christian Perez <perezchristian1012@gmail.com>
2020-01-19 00:29:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+    session[:name]
+  end
 
+  def logged_in?
+    !!current_user
   end
 end
