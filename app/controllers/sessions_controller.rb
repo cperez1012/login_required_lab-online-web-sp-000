f7217f358da52c4017944fcb27a2eebb2715bc08
Christian Perez <perezchristian1012@gmail.com>
2020-01-19 02:13:16 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    if params[:name].nil?
+      redirect_to '/login'
+    elsif params[:name].empty?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
 
   def destroy
